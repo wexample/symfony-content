0ddf1b314ca24a21fc1d3bd4df7ffca082d964bf
weeger <romain.weeger@gmail.com>
2026-07-24 11:26:25 +0200

    Cleanup

---
+++ src/Twig/MarkdownExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Wexample\SymfonyContent\Twig;
+
+use Symfony\Component\HttpKernel\KernelInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class MarkdownExtension extends AbstractExtension
+{
+    public function __construct(
+        private readonly KernelInterface $kernel,
+    ) {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('markdown_file', $this->markdownFile(...)),
+        ];
+    }
+
+    public function markdownFile(string $path): string
+    {
+        $fullPath = $this->kernel->getProjectDir().'/'.$path;
+
+        if (!file_exists($fullPath)) {
+            return '';
+        }
+
+        return file_get_contents($fullPath);
+    }
+}
